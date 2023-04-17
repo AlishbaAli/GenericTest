fdde2770b697d51b564515d2e080136526ba0a46
Alishba <alishba5ahmedali@gmail.com>
2023-04-17 19:35:55 -0400

    Add project files.

---
+++ GenericTest/GenericTestClass.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericTest
+{
+    public class GenericTestClass<T> where T : struct
+    {
+        private List<T> items = new List<T>();
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public List<T> GetDescendingItems()
+        {
+            items.Sort();
+            items.Reverse();
+            return items;
+        }
+    }
+}
